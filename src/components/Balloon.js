66d3965ac5d7618735aa93f885f5aa6f776812c0
moon <xmun74@naver.com>
2023-10-12 13:49:57 +0900

    feat: cra, tailwindcss setting

---
+++ src/components/Balloon.js
@@ -0,0 +1,4 @@
+const Balloon = () => {
+  return <div className="text-pink-500">풍선</div>;
+};
+export default Balloon;
---
+++ src/page/Main.js
@@ -0,0 +1,10 @@
+import Balloon from "../components/Balloon";
+
+const Main = () => {
+  return (
+    <div className="">
+      <Balloon />
+    </div>
+  );
+};
+export default Main;
